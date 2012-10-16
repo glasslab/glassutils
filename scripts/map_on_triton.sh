54b04987ebdddf6b99efb3ba240d3ef76cfccd2c
karmel <karmel@arcaio.com>
2012-10-16 11:00:45 -0700

    - Adding script for Triton mapping.

--- scripts/map_on_triton.sh
+++ scripts/map_on_triton.sh
@@ -21,7 +21,7 @@ fi
 
 if [ "$CMD" == "" ] || [ "$CMD" == "help" ]; then
 	echo "Please enter a command: send, clean, tophat, bowtie."
-	curr_file = `basename ${0}`
+	curr_file=`basename ${0}`
 	echo "Usage: ${curr_file} <1: send|clean|tophat|bowtie > <2: path to data on biowhat > <3: where to put data on Triton > <4: genome > <5: username on biowhat > <6: email address >"
 	echo "For help, please contact karmelallison@ucsd.edu"
 	exit
