4b44a11be9b95f4df223aa19e13cdc4614123918
karmel <karmel@arcaio.com>
2012-10-16 09:53:04 -0700

    - Adding script for Triton mapping.

---
+++ scripts/map_on_triton.sh
@@ -0,0 +1,94 @@
+CMD=$1
+GET_DIR=$2
+TO_DIR=$3
+GENOME=$4
+BIOWHAT_USER=$5
+EMAIL=$6
+
+EXEC_DIR=/projects/glass-lab/bioinformatics
+BOWTIE_INDEXES=/projects/glass-lab/bioinformatics/bowtie2/indexes
+GTF_FILES=/projects/glass-lab/bioinformatics/tophat2/gtf
+
+if [ "$GENOME" == "" ]; then
+  GENOME="mm9"
+fi
+if [ "$BIOWHAT_USER" == "" ]; then
+  BIOWHAT_USER="$USER"
+fi
+if [ "$EMAIL" == "" ]; then
+  EMAIL="$USER@ucsd.edu"
+fi
+
+# Get File locations
+DATA_DIR=$TO_DIR/`basename $GET_DIR`
+
+if [ "$CMD" == "send"]; then
+	echo "Copying ${DATA_DIR} to ${BIOWHAT_USER}@biowhat.ucsd.edu:${GET_DIR}..."
+	scp -r $DATA_DIR/*/*${GENOME}* $BIOWHAT_USER@biowhat.ucsd.edu:$GET_DIR 
+	exit
+else
+	if [ "$CMD" == "clean"]; then
+		if [ "$DATA_DIR" == ""]; then
+			echo "Cannot delete specified directory."
+			exit
+		fi
+		echo -n "Are you sure you want to remove the entire directory ${DATA_DIR}? [Y/n]"
+		read -e CONFIRM
+		if [ "$CONFIRM" == "Y" ] || [ "$CONFIRM" == "y" ] || [ "$CONFIRM" == "yes" ]; then
+			echo "Deleting ${DATA_DIR}."
+			#rm -r $DATA_DIR
+			exit
+		else
+			echo "Not deleting ${DATA_DIR}. Exiting."
+			exit
+		fi
+	else
+		# Set up operation
+		if [ "$CMD" == "tophat"]; then
+			OP = "perl ${EXEC_DIR}/map-bowtie2.pl -index ${BOWTIE_INDEXES}/${GENOME} -cpu 8 -p 1 --library-type fr-secondstrand -G ${GTF_FILES}/${GENOME}.refseq.gtf -tophat2 ${fastq}"
+		else
+			if [ "$CMD" == "bowtie"]; then
+				OP = "perl ${EXEC_DIR}/map-bowtie2.pl -index ${BOWTIE_INDEXES}/${GENOME} -cpu 1 -p 8 ${fastq}"
+			else
+				echo "Did not recognize command '${CMD}'. Exiting."
+				exit
+			fi
+		fi
+		
+		# Move files over
+		scp -r $BIOWHAT_USER@biowhat.ucsd.edu:$GET_DIR $TO_DIR 
+
+
+
+		# Decompress files, combine.
+		for SUB_DIR in $DATA_DIR/*
+		  do
+		    bname=`basename $SUB_DIR`
+		    zcat $SUB_DIR/*.gz > $SUB_DIR/$bname.fq
+		  done
+
+		# Create PBS file for each
+		for fastq in $DATA_DIR/*/*.fq
+		  do
+			bname_fq=`basename $fastq`
+			job_file=$DATA_DIR/${bname_fq}_job_file.sh
+			echo "#!/bin/bash
+			#PBS -q small
+			#PBS -N ${bname_fq}
+			#PBS -l nodes=1:ppn=8
+			#PBS -l walltime=10:00:00
+			#PBS -o ${fastq}.torque_output.txt
+			#PBS -e ${fastq}.torque_error.txt
+			#PBS -V
+			#PBS -M ${EMAIL}
+			#PBS -m abe
+
+			cd /phase1/${USER}
+
+			${OP}" > $job_file
+
+			qsub $job_file
+		done
+		exit
+	fi
+fi
