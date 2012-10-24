5b49b8cee43b0d55b548d45932a3fb4393054f56
Karmel Allison <karmel@arcaio.com>
2012-10-24 09:24:33 -0700

    - Increasing tophat walltime

--- scripts/map_on_triton.sh
+++ scripts/map_on_triton.sh
@@ -63,7 +63,7 @@ else
 		# Set up operation
 		if [ "$CMD" == "tophat" ]; then
 			OP="perl ${EXEC_DIR}/misc/map-bowtie2.pl -index ${BOWTIE_INDEXES}/${GENOME} -cpu 1 -p 1 --library-type fr-secondstrand -G ${GTF_FILES}/${GENOME}.refseq.gtf -tophat2 "
-			WTIME="10:00:00"
+			WTIME="20:00:00"
 			NODES="nodes=1:ppn=1"
 		else
 			if [ "$CMD" == "bowtie" ]; then
