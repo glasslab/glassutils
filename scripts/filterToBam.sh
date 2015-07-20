a62f4c68159e58a0fd650ee3d46d2469a85e5dc0
Jenhan Tao <jenhantao@gmail.com>
2015-07-19 23:00:32 -0700

    modified script to allow for qsub submission

--- scripts/filterToBam.sh
+++ scripts/filterToBam.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
         echo "Illegal number of parameters"
-        echo "filterToBam.sh <directory containing directories that contain sam/bam files> <output output directory>"
+        echo "filterToBam.sh <directory containing directories that contain sam/bam files> <output output directory> <email>"
         exit 1
 fi
 
 inputDir=$1
 outputDir=$2
+email=$3
 
 # make output directory if it doesn't exist
 if [ ! -d $outputDir ]
@@ -17,24 +18,57 @@ fi
 
 # loop for sam files
 find $inputDir -name '*.sam' | while read samFile; do
+	sampleName=${samFile/.sam/}
+	sampleName=${sampleName##*/}
 	bamFile=${samFile/.sam/.nodups}
 	bamFile=${bamFile##*/}
 	sortedFile=${samFile/.sam/.sorted}
 	sortedFile=${sortedFile##*/}
 	echo "samtools sort $samFile $outputDir/$sortedFile"
-	samtools sort $samFile $outputDir/$sortedFile
-	echo "samtools rmdup -s $outputDir/$sortedFile $outputDir/$bamFile"
-	samtools rmdup -s $outputDir/$sortedFile $outputDir/$bamFile
+	echo "samtools rmdup -s $outputDir/${sortedFile}.bam $outputDir/$bamFile"
+	command="samtools sort $samFile $outputDir/$sortedFile"
+	command="$command; samtools rmdup -s $outputDir/${sortedFile}.bam $outputDir/$bamFile"
+        echo "#!/bin/bash
+#PBS -q hotel
+#PBS -N $sampleName
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=3:00:00
+#PBS -o ${sampleName}_torque_output.txt
+#PBS -e ${sampleName}_torque_error.txt
+#PBS -V
+#PBS -M $email
+#PBS -m abe
+#PBS -A glass-group
+$command" > $outputDir/${sampleName}.torque.sh
+
+qsub $outputDir/${sampleName}.torque.sh
+
 done
 
 # loop for bam files
 find $inputDir -name '*.bam' | while read samFile; do
+	sampleName=${samFile/.sam/}
+	sampleName=${sampleName##*/}
 	bamFile=${samFile/.bam/.nodups}
-	bamFile=${samFile##*/}
+	bamFile=${bamFile##*/}
 	sortedFile=${samFile/.bam/.sorted}
 	sortedFile=${sortedFile##*/}
 	echo "samtools sort $samFile $outputDir/$sortedFile"
-	samtools sort $samFile $outputDir/$sortedFile
-	echo "samtools rmdup -s $outputDir/$sortedFile $outputDir/$bamFile"
-	samtools rmdup -s $outputDir/$sortedFile $outputDir/$bamFile
+	echo "samtools rmdup -s $outputDir/${sortedFile}.bam $outputDir/$bamFile"
+	command="samtools sort $samFile $outputDir/$sortedFile"
+	command="$command; samtools rmdup -s $outputDir/${sortedFile}.bam $outputDir/$bamFile"
+        echo "#!/bin/bash
+#PBS -q hotel
+#PBS -N $sampleName
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=3:00:00
+#PBS -o ${sampleName}_torque_output.txt
+#PBS -e ${sampleName}_torque_error.txt
+#PBS -V
+#PBS -M $email
+#PBS -m abe
+#PBS -A glass-group
+$command" > $outputDir/${sampleName}.torque.sh
+
+qsub $outputDir/${sampleName}.torque.sh
 done
