5b0d079f8872172d7ed1e7e9a070b0299146fc69
Jenhan Tao <jenhantao@gmail.com>
2015-07-20 11:02:40 -0700

    added script for getting only uniquely mapped reads from bam file

---
+++ scripts/getUniqueReads.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+if [ "$#" -ne 2 ]; then
+        echo "Illegal number of parameters"
+        echo "filterToBam.sh <directory containing directories that contain sam/bam files> <output output directory>"
+        exit 1
+fi
+
+inputDir=$1
+outputDir=$2
+
+# make output directory if it doesn't exist
+if [ ! -d $outputDir ]
+then
+	mkdir $outputDir
+fi
+
+
+# loop for sam files
+find $inputDir -name '*.sam' | while read samFile; do
+	sampleName=${samFile/.sam/}
+	sampleName=${sampleName##*/}
+
+	echo "samtools view -bq 1 $samFile > $outputDir/${sampleName}.unique.bam"
+	command="samtools view -bq 1 $samFile > $outputDir/${sampleName}.unique.bam"
+	        # create qsub file
+        echo "#!/bin/bash
+#PBS -q hotel
+#PBS -N $sampleName
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=3:00:00
+#PBS -o ${sampleName}_torque_output.txt
+#PBS -e ${sampleName}_torque_error.txt
+#PBS -V
+#PBS -M $email
+#PBS -m abe
+#PBS -A glass-group
+$command" > $outputDir/${sampleName}.torque.sh
+
+qsub $outputDir/${sampleName}.torque.sh
+done
+
+done
+
+# loop for bam files
+find $inputDir -name '*.bam' | while read bamFile; do
+	sampleName=${bamFile/.bam/}
+	sampleName=${sampleName##*/}
+
+	echo "samtools view -bq 1 $bamFile > $outputDir/${sampleName}.unique.bam"
+	command="samtools view -bq 1 $bamFile > $outputDir/${sampleName}.unique.bam"
+        # create qsub file
+        echo "#!/bin/bash
+#PBS -q hotel
+#PBS -N $sampleName
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=3:00:00
+#PBS -o ${sampleName}_torque_output.txt
+#PBS -e ${sampleName}_torque_error.txt
+#PBS -V
+#PBS -M $email
+#PBS -m abe
+#PBS -A glass-group
+$command" > $outputDir/${sampleName}.torque.sh
+
+qsub $outputDir/${sampleName}.torque.sh
+done
+
+done
