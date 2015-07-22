cd287afa064ebe3b5117e907283014c0d24c2781
Jenhan Tao <jenhantao@gmail.com>
2015-07-22 14:32:58 -0700

    updated script to make a renamed copy of log file; minor fixes for tscc changes

--- mapping_scripts/map-star.pl
+++ mapping_scripts/map-star.pl
@@ -3,7 +3,7 @@
 use strict;
 #Script to map with STAR
 
-my $starDir = "/projects/ps-glasslab/bioinformatics/software/STAR/";
+my $starDir = "/projects/glass-group/bioinformatics/STAR/";
 
 my $genome = $ARGV[0];
 my $file = $ARGV[1];
@@ -18,6 +18,12 @@ for(my $i = 1; $i < @split - 2; $i++) {
 my $command = $starDir . "STAR --genomeDir " . $starDir . "genomes/" . $genome . " --readFilesIn " . $file . " --runThreadN 4";
 `$command`;
 
+# rename aligned sam file
 $command = "mv " . $wd . "/" . $split[-2] . "/Aligned.out.sam " . $wd . "/" . $split[-2] . "/" . $split[-1] . "." . $genome . ".STAR.sam";
+print $command . "\n";
+`$command`;
+
+# make a renamed copy of log file
+$command = "cp " . $wd . "/" . $split[-2] . "/Log.final.out " . $wd . "/" . $split[-2] . "/" . $split[-1] . "." . $genome . ".Log.final.out";
 print $command . "\n";
 `$command`;
