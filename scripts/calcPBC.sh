66f170428c9f993691092e03f713e939547b3bc9
Jenhan Tao <jenhantao@gmail.com>
2015-07-31 17:11:09 -0700

    removed unused calcSpot script; began consolidating calcPBC and calulatePileups

--- scripts/calcPBC.sh
+++ scripts/calcPBC.sh
@@ -2,16 +2,78 @@
 
 # parse the input
 inputDir=$1
+outputDir=$2
+email=$3
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 3 ]; then
 	echo "Illegal number of parameters"
-	echo "calcPBC.sh <directory containing samtool pileups> "
+	echo "calculatePileups.sh <directory that contains sam/bam files> <outputDirectory> <email>"
 	exit 1
 fi
 
-find $inputDir -name '*.pileup' | while read pileupFile; do
-	sampleName=${pileupFile%%.*}
+# make directory to store  output
+if [ ! -d $outputDir ]
+then
+	mkdir $outputDir
+fi
+
+find $inputDir -name '*.sam' | while read samFile; do
+	sampleName=${samFile/.fastq.mm10.bowtie2.sam/}
 	sampleName=${sampleName##*/}
-	PBC=$(awk 'BEGIN {N1=0;ND=0} {if($4==1){N1+=1} ND+=1} END{print N1/ND}' ${pileupFile})
-	echo -e "$sampleName\t$PBC"
+
+	sortedFile=$outputDir/${sampleName}.sorted
+
+	pileupFile=$outputDir/${sampleName}.pileup
+
+	command="samtools sort $samFile $sortedFile"
+	command+="; samtools mpileup ${sortedFile}.bam > $pileupFile"
+
+	echo "submitting $command"
+
+	# create qsub file
+	echo "#!/bin/bash
+#PBS -q hotel
+#PBS -N $sampleName
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=3:00:00
+#PBS -o ${sampleName}_torque_output.txt
+#PBS -e ${sampleName}_torque_error.txt
+#PBS -V
+#PBS -M $email
+#PBS -m abe
+#PBS -A glass-group
+$command" > $outputDir/${sampleName}.torque.sh
+
+qsub $outputDir/${sampleName}.torque.sh
+done
+
+
+find $inputDir -name '*.bam' | while read bamFile; do
+	sampleName=${bamFile/.fastq.mm10.bowtie2.bam/}
+	sampleName=${sampleName##*/}
+
+	sortedFile=$outputDir/${sampleName}.sorted
+
+	pileupFile=$outputDir/${sampleName}.pileup
+
+	command="samtools sort $bamFile $sortedFile"
+	command+="; samtools mpileup ${sortedFile}.bam > $pileupFile"
+
+	echo "submitting $command"
+
+	# create qsub file
+	echo "#!/bin/bash
+#PBS -q hotel
+#PBS -N $sampleName
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=1:00:00
+#PBS -o ${sampleName}_torque_output.txt
+#PBS -e ${sampleName}_torque_error.txt
+#PBS -V
+#PBS -M $email
+#PBS -m abe
+#PBS -A glass-group
+$command" > $outputDir/${sampleName}.torque.sh
+
+qsub $outputDir/${sampleName}.torque.sh
 done
